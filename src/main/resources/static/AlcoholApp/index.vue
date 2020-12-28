8deb2e5d590b5ce7e4ca178676084e80ef62b318
19070 <1907067365@qq.com>
2020-12-28 16:09:41 +0800

    10.1.2 APP:null 后台:修复了后台点击酒模板会返回登录界面的bug 备注:null

---
+++ src/main/resources/static/AlcoholApp/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <view class="container">
+        <view class="shuru">
+            <form>
+                <text>请输入需要定制的酒名</text>
+                <input>
+            </form>
+        </view>
+    </view>
+</template>
+
+<script>
+    export default {
+        created() {
+            // location.href="https://38482.165183.com/BuildWeb/#/?pagename=index&vv=59"
+        },
+        data() {
+        },
+        methods: {
+            test: function () {
+                location.href = "http://38482.kyk221.cn/BuildWeb/#/pages/product/productdetail?proid=1385714"
+            },
+        }
+    }
+</script>
+
+<style>
+    .container {
+        padding: 20px;
+        font-size: 14px;
+        line-height: 24px;
+    }
+
+    .shuru {
+        text-align: center;
+    }
+</style>
